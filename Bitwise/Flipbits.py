b1a8f78950de2ec04ac239e0f5598ef220ccf930
Kishan Mishra <90112328+KishanMishra1@users.noreply.github.com>
2022-02-18 19:43:55 +0530

    Create Flipbits.py

---
+++ Bitwise/Flipbits.py
@@ -0,0 +1,13 @@
+bit_s = '1010'
+inverse_s = ''
+  
+for i in bit_s:
+    
+    if i == '0':
+        inverse_s += '1'
+          
+    else:
+        inverse_s += '0'
+          
+print("Inversed string is ",
+      inverse_s)
